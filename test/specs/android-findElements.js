0467e65c635fd94a3cd88aa23689ee91509a90db
Jamil Kawsher <jamilkawsher@gmail.com>
2023-10-14 20:49:03 +0600

    Find by xpath,resource-id,Assertions Added

--- test/specs/android-findElements.js
+++ test/specs/android-findElements.js
@@ -11,11 +11,35 @@ describe("Android Element", () => {
     await expect(actionBar).toBeExisting();
   });
 
-  it.only("Find Element By Class Name ", async () => {
+  it("Find Element By Class Name ", async () => {
     const className = await $("android.widget.TextView");
     // console.log(await className.getText());
     //Assertion
 
     expect(className).toHaveText("API Demo");
+  });
+
+  it("Find Element By Xpath", async () => {
+    //Find Element using Xpath
+
+    //xpath - (//tagname[@attribute=value])
+    (
+      await $("//android.widget.TextView[@content-desc='Alert Dialogs']")
+    ).click();
+
+    //Find Element Using resource-id
+    //xpath - (//tagname[@attribute=value])
+    await $(
+      "//android.widget.Button[@resource-id='io.appium.android.apis:id/select_button']"
+    ).click();
+
+    //Find Element By text
+
+    (await $("//android.widget.TextView[@text='Command two']")).click();
+
+    // Find By Text And -----Assertion
+    const textAssertion = await $("android.widget.TextView");
+
+    expect(textAssertion).toHaveText("You selected: 1 , Command two");
   });
 });
