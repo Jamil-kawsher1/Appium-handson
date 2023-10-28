557a48df314f0228e8ceb57825f0ac22514d2d4c
Jamil kawsher <jamilkawsher@gmail.com>
2023-10-28 21:05:29 +0600

    Directly access alert activity and then used default method to Dismiss

--- test/specs/dialogAndAlertBox.js
+++ test/specs/dialogAndAlertBox.js
@@ -1,6 +1,6 @@
 describe("Dialog and Alert", () => {
 
-    it("Working With Dialog box", async () => {
+    it("Working With Dialog box auto Accept", async () => {
 
         //path APP ->Alert Dialog-> Ok Cancel Dialog With Message
 
@@ -24,6 +24,27 @@ describe("Dialog and Alert", () => {
         //assert That Alert text Is Gone
 
         await expect($("//*[@resource-id='android: id / alertTitle']")).not.toExist()
+
+    })
+
+    it.only("Dialog box Dismiss using In built method ", async () => {
+
+        //Directly Accessing Alert Dialog Activity/Screen
+
+        await driver.startActivity("io.appium.android.apis", "io.appium.android.apis.app.AlertDialogSamples")
+
+        // Clicking On Ok Cancel Dialog with a Message
+
+        await $("//*[@resource-id='io.appium.android.apis:id/two_buttons']").click()
+
+        //Dismissing The Alert using Webedriver IoS Native mathod
+
+        await driver.dismissAlert()
+
+        // Checking For Dismiss is Executed Or Not
+
+        await expect($("//*[@resource-id='android: id / alertTitle']")).not.toExist();
+
 
     })
 })
