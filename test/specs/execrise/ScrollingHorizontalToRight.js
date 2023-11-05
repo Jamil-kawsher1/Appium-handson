9492d7630ab1354840900eb5508c0bdfcabe602c
Jamil kawsher <jamilkawsher@gmail.com>
2023-11-05 23:42:51 +0600

    Date Picker date pic with right horizintal scrolling

--- test/specs/execrise/ScrollingHorizontalToRight.js
+++ test/specs/execrise/ScrollingHorizontalToRight.js
@@ -1,6 +1,24 @@
 describe("Scroll Horizontally to Right", () => {
 
-    it("Sroll Horizontally to right and pick the 10th date of new month")
-    // view -> Data Widget->Dialog
+    it("Sroll Horizontally to right and pick the 10th date of new month", async () => {
+
+        // view -> Data Widget->Dialog
+        await $("~Views").click();
+
+        await $("~Date Widgets").click();
+
+        await $("~1. Dialog").click();
+
+        await $("~change the date").click();
+
+        await $('android=new UiScrollable(new UiSelector().scrollable(true)).setAsHorizontalList().scrollForward()');
+
+        await browser.pause(4000)
+        await $('//android.view.View[@content-desc="10 December 2023"]').click()
+    })
+
+
+
+
 
 })
