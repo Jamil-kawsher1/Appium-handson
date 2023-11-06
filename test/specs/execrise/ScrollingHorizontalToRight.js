8ec7d0edf14655a43420242f55d1553d19863967
Jamil kawsher <jamilkawsher@gmail.com>
2023-11-06 23:09:42 +0600

    Code optimized and bug fixes for scrolling and Assertion

--- test/specs/execrise/ScrollingHorizontalToRight.js
+++ test/specs/execrise/ScrollingHorizontalToRight.js
@@ -9,12 +9,29 @@ describe("Scroll Horizontally to Right", () => {
 
         await $("~1. Dialog").click();
 
+        //get current date
+
+        let currentDate = await $('//android.widget.TextView[@resource-id="io.appium.android.apis:id/dateDisplay"]').getText()
         await $("~change the date").click();
 
         await $('android=new UiScrollable(new UiSelector().scrollable(true)).setAsHorizontalList().scrollForward()');
 
         await browser.pause(4000)
-        await $('//android.view.View[@content-desc="10 December 2023"]').click()
+        // await $('//android.view.View[@content-desc="10 December 2023"]').click()
+
+        await $("//*[@text='10']").click()
+
+
+        await $("//*[@text='OK']").click()
+
+        browser.pause(4000)
+
+        expect(currentDate).not.toEqual(currentDate2)
+
+
+
+
+
     })
 
 
